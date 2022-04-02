aa3e93fa80b343580b7c433fc66a2f5d6d4e4097
Mah E Urooj <102211657+MahEUrooj@users.noreply.github.com>
2022-04-02 13:50:48 +0500

    Add files via upload

---
+++ dazzling_borwo1 (1).ino
@@ -0,0 +1,53 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  
+ 
+}
+
+void loop()
+{
+  digitalWrite(0, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(0, LOW);
+  delay(500); 
+  digitalWrite(1, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(1, LOW);
+  delay(500);
+  digitalWrite(2, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(500);
+  digitalWrite(3, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(3, LOW);
+  delay(500); // Wait for  
+  digitalWrite(4, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(4, LOW);
+  delay(500);
+  digitalWrite(3, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(3, LOW);
+  delay(500); // Wait for 
+  delay(500); // Wait for 
+  digitalWrite(2, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(500); 
+  digitalWrite(1, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(1, LOW);
+  delay(500);
+  digitalWrite(0, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(0, LOW);
+  delay(500);
+}
